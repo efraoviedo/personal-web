3232b5bf272aa081419d7436860de900e32469a1
efranor <efraoviedo@gmail.com>
2024-01-15 07:14:51 -0500

    updating bg of the coments

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -82,7 +82,7 @@ const CommentsForm = ({ slug }) => {
   };
 
   return (
-    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+    <div className="bg-cyan-100 shadow-lg rounded-lg p-8 pb-12 mb-8">
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">
         Deja un Comentario
       </h3>
